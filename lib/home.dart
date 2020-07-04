de901fb8f40dc3e8a7fc7550e0ae1b68f37f5ee9
Lokesh7688 <lokesh0kashyap@gmail.com>
2020-07-04 08:01:19 -0700

    Posts

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:bottom_nav_flutter/posts.dart';
 import 'package:flutter/material.dart';
 import 'usersstories.dart';
 
@@ -34,6 +35,41 @@ class _homeState extends State<home> {
         "Narendra Modi"),
   ];
 
+  List<post> posts = [
+    post(
+      Username: "Narendramodi",
+      UserImage:
+          "https://pbs.twimg.com/profile_images/1249934092994375680/xX66NSZP_400x400.jpg",
+      PostedImage:
+          "https://instagram.fjai1-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s750x750/106470626_683362015839983_8411456334679807048_n.jpg?_nc_ht=instagram.fjai1-2.fna.fbcdn.net&_nc_cat=1&_nc_ohc=Qihxfe5hRmUAX9835jO&oh=eb0f10e8bcf5d63a02bdef3ad008f5f2&oe=5F29AC8E",
+      ImageText: "Indian Army is Love",
+    ),
+    post(
+      Username: "ShashiKant",
+      UserImage:
+          "https://pbs.twimg.com/profile_images/1249934092994375680/xX66NSZP_400x400.jpg",
+      PostedImage:
+          "https://pbs.twimg.com/profile_images/1247567101616746496/YuhgEjc__400x400.jpg",
+      ImageText: "Indian Army is Love",
+    ),
+    post(
+      Username: "Harsh_Khatri",
+      UserImage:
+          "https://pbs.twimg.com/profile_images/1249934092994375680/xX66NSZP_400x400.jpg",
+      PostedImage:
+          "https://pbs.twimg.com/profile_images/1249934092994375680/xX66NSZP_400x400.jpg",
+      ImageText: "Indian Army is Love",
+    ),
+    post(
+      Username: "Rohit_Goyal",
+      UserImage:
+          "https://pbs.twimg.com/profile_images/1249934092994375680/xX66NSZP_400x400.jpg",
+      PostedImage:
+          "https://pbs.twimg.com/profile_images/1249934092994375680/xX66NSZP_400x400.jpg",
+      ImageText: "Indian Army is Love",
+    ),
+  ];
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -65,11 +101,6 @@ class _homeState extends State<home> {
           child: Column(
             children: <Widget>[
               Container(
-                margin: EdgeInsets.symmetric(
-                  horizontal: 10,
-                ),
-              ),
-              Container(
                 width: MediaQuery.of(context).size.width,
                 height: 100,
                 margin: EdgeInsets.symmetric(
@@ -90,7 +121,7 @@ class _homeState extends State<home> {
                                 borderRadius: BorderRadius.circular(60.0),
                                 border: Border.all(
                                   width: 2,
-                                  color: Color(0xff8e44ff),
+                                  color: Color(0xff8e4445),
                                 )),
                             child: Container(
                               padding: EdgeInsets.all(
@@ -122,6 +153,47 @@ class _homeState extends State<home> {
                       );
                     },
                   ).toList(),
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: posts.length,
+                  itemBuilder: (context, index) => Container(
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 10),
+                          child: Row(
+                            children: <Widget>[
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(40.0),
+                                child: Image(
+                                  image: NetworkImage(posts[index].UserImage),
+                                  width: 40,
+                                  height: 40,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 10.0,
+                              ),
+                              Text(posts[index].Username),
+                            ],
+                          ),
+                        ),
+                        Image(
+                          image: NetworkImage(
+                            posts[index].PostedImage,
+                          ),
+                          width: MediaQuery.of(context).size.width,
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ),
             ],
---
+++ lib/posts.dart
@@ -0,0 +1,13 @@
+class post {
+  final String Username;
+  final String UserImage;
+  final String ImageText;
+  final String PostedImage;
+
+  post({
+    this.ImageText,
+    this.PostedImage,
+    this.UserImage,
+    this.Username,
+  });
+}
